bb5ed6f39d4bdd646667af0c2f592e4d8fa89ceb
Sean Cosentino <Sparkcollaboration@gmail.com>
2015-09-26 15:37:36 -0400

    Hot fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-window.onload = function() {
-    if(!window.location.hash) {
-        window.location = window.location + '#loaded';
-        window.location.reload();
-    }
-};
